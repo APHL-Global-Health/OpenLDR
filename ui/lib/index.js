a22cce44eccdd214992f1c42cfab2b57d877d9ee
Fredrick Mwasekaga <fmwasekaga@users.noreply.github.com>
2021-12-30 11:13:56 +0300

    Update index.js

--- ui/lib/index.js
+++ ui/lib/index.js
@@ -3,11 +3,13 @@ const mongo_helper =  require('./mongo_helper');
 const Server = require("./server");
 const WebSocket = require('./web_socket');
 const Project = require('./project');
+const Parser = require('./sql-parse/parser');
 
 module.exports = {
     logger,
     mongo_helper,
     Server,
     WebSocket,
-    Project
+    Project,
+    Parser
 }
